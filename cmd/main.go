fb6832c4249149ed977011fe53252c995dc5592d
Tashwin SJ <tashwinsj@Tashwins-MacBook-Air.local>
2025-04-24 17:57:22 +0530

    Logging functionality

--- cmd/main.go
+++ cmd/main.go
@@ -4,24 +4,33 @@ import (
 	"fmt"
 	"net"
 	"os" 
-	"log"
-	"os/signal"
+ 	"os/signal"
 	"realDB/internal/server"
 	"syscall"
 	"time"
+	"github.com/sirupsen/logrus"
 ) 
 
 var (
 	shutdownChan = make(chan struct{})
 )
 
+func init() {
+	file, err := os.OpenFile("realdb.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		logrus.SetOutput(file)
+	} else {
+		logrus.Info("Failed to log to file, using default stderr")
+	}
+}
 
 func main(){
 	ln , err := net.Listen("tcp" , ":6369") 
 	if err != nil {
 		panic(err)
 	} 
-	fmt.Println("Server started on port 6369")  
+	fmt.Println("Server started on port 6369")   
+	logrus.Info("Server Started on port 6369")
 
 	go HandleShutDown(ln)
 
@@ -31,11 +40,11 @@ func main(){
 				select {
 					case <-shutdownChan:
 						// Expected error during shutdown — suppress or log cleanly
-						log.Println("Stopped accepting new connections (listener closed).")
+						logrus.Info("Stopped accepting new connections (listener closed).")
 						return
 					default:
 						// Unexpected error — log it
-						log.Printf("Connection error : %v", err)
+						logrus.Errorf("Connection error : %v", err)
 						continue
 				}
 		} 
@@ -52,8 +61,18 @@ func main(){
 |                                                           |
 |                  Connected to real-db>                    |
 '-----------------------------------------------------------'` + "\n")) 
-		conn.Write([]byte("real-db> "))
+		conn.Write([]byte("real-db> "))  
 
+		remoteAddress := conn.RemoteAddr() 
+		if tcpAddr, ok := remoteAddress.(*net.TCPAddr); ok {
+			ipAddress := tcpAddr.IP
+			portNumber := tcpAddr.Port
+			logrus.Infof("New client connected IP: %s  PORT: %d", ipAddress, portNumber)
+		} else {
+			logrus.Warn("New client connected, could not cast the remote address of the connected client")
+		}
+
+ 
 		go server.HandleConnection(conn)      // This infinite loop keeps looking for connections and wherever they come a seperate go routine is 
 										// created to handle the request ( seperate go routine for every client connected )
 	}
@@ -66,7 +85,7 @@ func HandleShutDown(listener net.Listener) {
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
 	sig := <-sigChan
-	log.Printf("Received signal: %s. Shutting down gracefully... \n", sig)
+	logrus.Infof("Received signal: %s. Shutting down gracefully!", sig)
 
 	// Stop new connections
 	close(shutdownChan)
@@ -76,6 +95,7 @@ func HandleShutDown(listener net.Listener) {
 	time.Sleep(1 * time.Second) // adjust as needed
 
 	// Clean up resources (watchers, etc.)
-	log.Printf("Graceful shutdown complete.")
-	os.Exit(0)
+	logrus.Info("Graceful shutdown complete.")
+	time.Sleep(1000 * time.Millisecond)
+	logrus.Exit(0)
 }
